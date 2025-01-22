94bb0cbe1cd6181d355438d6805b4b12ab59eb68
Kyan Jasani-Draper <kyanjd@gmail.com>
2025-01-22 18:20:07 +0000

    removed e and pi from equations as causing issues

--- fyp_work/paper_to_equation/Generation/Equation_BaseDataset.py
+++ fyp_work/paper_to_equation/Generation/Equation_BaseDataset.py
@@ -11,8 +11,8 @@ import re
 
 class Equation:
     # Class attributes (only instantiated the first time)
-    latin = symbols('a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z')
-    greek = symbols('α β γ δ ε ζ η θ ι κ λ μ ν ξ ο π ρ σ τ υ φ χ ψ ω Α Β Γ Δ Ε Ζ Η Θ Ι Κ Λ Μ Ν Ξ Ο Π Ρ Σ Τ Υ Φ Χ Ψ Ω')
+    latin = symbols('a b c d f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z')
+    greek = symbols('α β γ δ ε ζ η θ ι κ λ μ ν ξ ο ρ σ τ υ φ χ ψ ω Α Β Γ Δ Ε Ζ Η Θ Ι Κ Λ Μ Ν Ξ Ο Π Ρ Σ Τ Υ Φ Χ Ψ Ω')
     vars = latin + greek 
     operators = ('+', '-', '*', '/', '**')
     functions = (sin, cos, tan, exp, log, sqrt)
